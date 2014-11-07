9ca1b0ef4cac7f74b31f760b9c46f9c6da596f15
augre <augre.augre@gmail.com>
2014-11-07 09:39:55 -0600

    swap with arrays too

--- swap.c
+++ swap.c
@@ -13,6 +13,17 @@ int main(){
   printf("x is %d, y is %d\n", x, y);
   Swap(&x,&y);
   printf("x is %d, y is %d\n", x, y);
+  /*swap with arrays*/
+  int scores[10];
+  scores[0]=1;
+  scores[9]=2;
+  printf("scores[0] is %d, scores[9] is %d\n", scores[0], scores[9]);
+  Swap(&(scores[0]), &(scores[9]));
+  printf("scores[0] is %d, scores[9] is %d\n", scores[0], scores[9]);
+  /*other way to pass array pointers*/
+  Swap(scores, scores+9);
+  printf("scores[0] is %d, scores[9] is %d\n", scores[0], scores[9]);
   return 0;
   
-}
+} 
+
