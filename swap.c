926620d1de3f023365ec43abec453db22ab04464
root <root@debian>
2014-11-09 14:53:16 -0600

    added multi dim array, and example of using swap with string

--- swap.c
+++ swap.c
@@ -7,9 +7,25 @@ void Swap(int* a, int* b) {
   *b=temp;
 }
 
+void Swapc(char* a, char* b){
+  char temp;
+  temp=*a;
+  *a=*b;
+  *b=temp;
+}
+
+int g(int a[][2]){
+  a[2][0]=5;
+  printf("%d\n",a[2][0]);
+  return 0;
+}
+
 int main(){
   int x=1;
   int y=2;
+  char szoveg[10]="szoveg\n";
+  int a[3][2];
+
   printf("x is %d, y is %d\n", x, y);
   Swap(&x,&y);
   printf("x is %d, y is %d\n", x, y);
@@ -23,6 +39,24 @@ int main(){
   /*other way to pass array pointers*/
   Swap(scores, scores+9);
   printf("scores[0] is %d, scores[9] is %d\n", scores[0], scores[9]);
+
+  printf(szoveg);
+  Swapc(szoveg, szoveg+1);
+  printf(szoveg);
+
+  /*this is passing by reference too, because the effect 
+  of the funtion exists in the caller after the function exited*/
+  g(a);
+  /*this one works*/
+  printf("%d\n",a[2][0]);
+  /*these ones don't*/
+  printf("%d\n",a[4]);
+  printf("%d\n",a[2]+0);
+  printf("%d\n", *(a+2));
+  
+  
+
+
   return 0;
   
 } 
